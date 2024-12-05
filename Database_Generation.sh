f7a2a6f796bb95f9ad7dd5b56efb4f728047c145
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2024-12-05 02:36:11 -0600

    Update and rename MS_detected_proteins.sh to Database_Generation.sh

---
+++ Database_Generation.sh
@@ -0,0 +1,9 @@
+#This code generates a transcription-supported protein database for two MS datasets
+
+#Caglar2017 dataset - REL606
+#Mori2021 - K12 MG1655, K12 NCM3722. One is from Nissle, which we can ignore
+
+#Are these strains sufficiently similar to use ORFs generated from just one database?
+
+cp /stor/work/Ochman/hassan/protogene_extension/Ecoli_list/sequence_RS.fasta K12MG1655.faa
+cp /stor/work/Ochman/hassan/proteomics_denovo/04092024_compgenomics_final/GCF_000017985.1_ASM1798v1_genomic.fna REL606.faa
--- MS_detected_proteins.sh
+++
@@ -1,1 +0,0 @@
-#This code generates a list of non-annotated proteins detected from mass spectrometry data
