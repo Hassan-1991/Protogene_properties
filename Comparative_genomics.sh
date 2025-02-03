5c6237d53ccdaee3b4755adce8040932aa64f7e5
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2025-02-03 17:06:14 -0600

    Create Comparative_genomics.sh

---
+++ Comparative_genomics.sh
@@ -0,0 +1,4 @@
+#Take protogenes from E. coli, Salmonella and M. tuberculosis
+#Take "representative" samples of annotated genes (whatever that means) from all three
+#Run the comparative genomics pipeline - except add the without-synteny search as well
+
