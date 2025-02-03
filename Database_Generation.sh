c387d1d9030e200769cd69136688593f9a5f030f
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2025-02-03 13:39:05 -0600

    Update Database_Generation.sh

--- Database_Generation.sh
+++ Database_Generation.sh
@@ -10,32 +10,65 @@ cd /stor/scratch/Ochman/hassan/112724_protogene_extension/proteogenomics_databas
 
 cp /stor/work/Ochman/hassan/protogene_extension/Ecoli_list/sequence_RS.fasta NC_000913.3.faa
 cp /stor/work/Ochman/hassan/proteomics_denovo/04092024_compgenomics_final/GCF_000017985.1_ASM1798v1_genomic.fna NC_012967.1.faa
+/stor/work/Ochman/hassan/proteomics_denovo/8925_raw_data/ECOR_11_genome.faa .
+/stor/work/Ochman/hassan/proteomics_denovo/8925_raw_data/ECOR_27_genome.faa .
+/stor/work/Ochman/hassan/proteomics_denovo/8925_raw_data/ECOR_37_genome.faa .
 
-#Extract strain-specific annotation files
-#Ecoli_all_annotated.gtf was generated using the protogene_curation code
+#Annotate using prodigal, balrog, genemarks2
+#For some reason balrog doesn't run for the ECOR genomes
+for i in NC_000913.3.faa NC_012967.1.faa ECOR_11_genome.faa ECOR_27_genome.faa ECOR_37_genome.faa
+do echo "/stor/work/Ochman/hassan/tools/prod_gms_balrog.sh ${i}"
+done > running.sh
 
-for i in NC_012967.1 NC_000913.3
-do 
-sed "s/REL606/NC_012967.1/g" /stor/work/Ochman/hassan/protogene_extension/Ecoli_list/Ecoli_all_annotated.gtf | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4,$5,$6=".",$7,$8,$9}' | awk -F '\t' '($3=="CDS")' | grep -F "$i" > "$i".all_annotated.gtf
+#annotate with smorfinder
+
+conda activate smorfinder_env
+for i in NC_000913.3.faa NC_012967.1.faa ECOR_11_genome.faa ECOR_27_genome.faa ECOR_37_genome.faa
+do
+smorf single "$i" && mv smorf_output "$i"_smorf_output
 done
 
-for i in NC_012967.1 NC_000913.3
+#convert to gtf
+
+for i in NC_000913.3.faa NC_012967.1.faa ECOR_11_genome.faa ECOR_27_genome.faa ECOR_37_genome.faa
+do
+cut -f-8 "$i"*smorf_output/*gff | awk -F '\t' '{print $0,"\ttranscript_id \""$1"_smorfer_"NR"\";gene_id \""$1"_smorfer_"NR"\";"}' > "$i"_smorfer.gtf
+cut -f-8 "$i"*prodigal.gff | awk -F '\t' '{print $0,"\ttranscript_id \""$1"_prodigal_"NR"\";gene_id \""$1"_prodigal_"NR"\";"}' > "$i"_prodigal.gtf
+awk -F '\t' '($3=="CDS")' "$i"*genemarks2.gff | cut -f -8 | awk -F '\t' '{print $0,"\ttranscript_id \""$1"_gms2_"NR"\";gene_id \""$1"_gms2_"NR"\";"}' > "$i"_gms2.gtf
+done
+
+#Concatenate everything
+
+for i in NC_000913.3 NC_012967.1 ECOR_11_genome ECOR_27_genome ECOR_37_genome
+do
+cat "$i"*smorfer.gtf "$i"*prodigal.gtf "$i"*gms2.gtf "$i"*balrog.gtf  | grep -v "^#" | awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4,$5,".",$7,".",$9,$10}' > "$i"_annotated.gtf
+done
+
+for i in NC_000913.3 NC_012967.1 ECOR_11_genome ECOR_27_genome ECOR_37_genome
 do
 getorf -sequence "$i".faa -outseq "$i".getorf.bacterial -table 11 -minsize 30 -find 3
 done
 
+for i in NC_000913.3 NC_012967.1 ECOR_11_genome ECOR_27_genome ECOR_37_genome
+do
+grep "^>" "$i".getorf.bacterial | grep -v "REVERSE" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | awk '{OFS=""}{print $1"\t.\tCDS\t",$2,"\t",$4+3,"\t.\t+\t0\tgene_id \"",$1,"\";transcript_id \"",$1,"\";"}' | sed "s/_/\t/2" | cut -f1,3- > "$i".getorf.bacterial.gtf
+grep "^>" "$i".getorf.bacterial | grep "REVERSE" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | awk '{OFS=""}{print $1"\t.\tCDS\t",$4-3,"\t",$2,"\t.\t-\t0\tgene_id \"",$1,"\";transcript_id \"",$1,"\";"}' | sed "s/_/\t/2" | cut -f1,3- >> "$i".getorf.bacterial.gtf
+done
 
+cp NC_012967.1.faa REL606.faa
+cp NC_012967.1_annotated.gtf REL606_annotated.gtf
+cp NC_012967.1.getorf.bacterial.gtf REL606.getorf.bacterial.gtf
 
-grep "^>" REL606.getorf.bacterial | grep -v "REVERSE" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | awk '{OFS=""}{print $1"\t.\tCDS\t",$2,"\t",$4+3,"\t.\t+\t0\tgene_id \"",$1,"\";transcript_id \"",$1,"\";"}' | sed "s/_/\t/2" | cut -f1,3- > REL606.getorf.bacterial.gtf
-grep "^>" REL606.getorf.bacterial | grep "REVERSE" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | awk '{OFS=""}{print $1"\t.\tCDS\t",$4-3,"\t",$2,"\t.\t-\t0\tgene_id \"",$1,"\";transcript_id \"",$1,"\";"}' | sed "s/_/\t/2" | cut -f1,3- >> REL606.getorf.bacterial.gtf
+cp NC_000913.3.faa K12MG1655.faa
+cp NC_000913.3_annotated.gtf K12MG1655_annotated.gtf
+cp NC_000913.3.getorf.bacterial.gtf K12MG1655.getorf.bacterial.gtf
 
-grep "^>" K12MG1655.getorf.bacterial | grep -v "REVERSE" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | awk '{OFS=""}{print $1"\t.\tCDS\t",$2,"\t",$4+3,"\t.\t+\t0\tgene_id \"",$1,"\";transcript_id \"",$1,"\";"}' | sed "s/_/\t/2" | cut -f1,3- > K12MG1655.getorf.bacterial.gtf
-grep "^>" K12MG1655.getorf.bacterial | grep "REVERSE" | sed "s/\[//g" | sed "s/\]//g" | tr -d ">" | awk '{OFS=""}{print $1"\t.\tCDS\t",$4-3,"\t",$2,"\t.\t-\t0\tgene_id \"",$1,"\";transcript_id \"",$1,"\";"}' | sed "s/_/\t/2" | cut -f1,3- >> K12MG1655.getorf.bacterial.gtf
-
-awk -F '\t' '($7=="+")' REL606.getorf.bacterial.gtf |
+for i in K12MG1655 REL606 ECOR_11_genome ECOR_27_genome ECOR_37_genome
+do
+awk -F '\t' '($7=="+")' "$i".getorf.bacterial.gtf |
 awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4-500,$5,$6,$7,$8,$9}' |
 awk -F '\t' '{OFS=FS}{if ($4 < 1) $4 = 1; print}' |
-gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - |
+gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - |
 sed '/^>/!{ s/.*/echo "&" | rev/e }' |
 awk '{if ($0 ~ /^>/) print; else {gsub(/.{3}/, "& "); print}}' |
 cut -f2- -d " " |
@@ -43,12 +76,12 @@ sed -E 's/AGT|AAT|GAT/%/g' |
 seqkit fx2tab | cut -f1 -d "%" |
 sed -E 's/GTA|GTG|GTT/@/g' | grep "@" |
 rev | cut -f2- -d "@" | rev |
-sed "s/@/NNN/g" | sed "s/ //g" | awk -F '\t' '{print $1,length($2)+6}' | sed "s/ /\t/g" > REL606.getorf.bacterial.longestORFlengths.tsv
+sed "s/@/NNN/g" | sed "s/ //g" | awk -F '\t' '{print $1,length($2)+6}' | sed "s/ /\t/g" > "$i".getorf.bacterial.longestORFlengths.tsv
 
-awk -F '\t' '($7=="-"&&$4>0)' REL606.getorf.bacterial.gtf |
+awk -F '\t' '($7=="-"&&$4>0)' "$i".getorf.bacterial.gtf |
 awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4,$5+500,$6,$7,$8,$9}' |
 awk -F '\t' '{OFS=FS}{if ($5 > 4629812) $5 = 4629812; print}' |
-gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - |
+gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - |
 sed '/^>/!{ s/.*/echo "&" | rev/e }' |
 awk '{if ($0 ~ /^>/) print; else {gsub(/.{3}/, "& "); print}}' |
 cut -f2- -d " " |
@@ -56,93 +89,31 @@ sed -E 's/AGT|AAT|GAT/%/g' |
 seqkit fx2tab | cut -f1 -d "%" |
 sed -E 's/GTA|GTG|GTT/@/g' | grep "@" |
 rev | cut -f2- -d "@" | rev |
-sed "s/@/NNN/g" | sed "s/ //g" | awk -F '\t' '{print $1,length($2)+6}' | sed "s/ /\t/g" >> REL606.getorf.bacterial.longestORFlengths.tsv
+sed "s/@/NNN/g" | sed "s/ //g" | awk -F '\t' '{print $1,length($2)+6}' | sed "s/ /\t/g" >> "$i".getorf.bacterial.longestORFlengths.tsv
+done
 
-awk -F '\t' '($7=="+")' K12MG1655.getorf.bacterial.gtf |
-awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4-500,$5,$6,$7,$8,$9}' |
-awk -F '\t' '{OFS=FS}{if ($4 < 1) $4 = 1; print}' |
-gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - |
-sed '/^>/!{ s/.*/echo "&" | rev/e }' |
-awk '{if ($0 ~ /^>/) print; else {gsub(/.{3}/, "& "); print}}' |
-cut -f2- -d " " |
-sed -E 's/AGT|AAT|GAT/%/g' |
-seqkit fx2tab | cut -f1 -d "%" |
-sed -E 's/GTA|GTG|GTT/@/g' | grep "@" |
-rev | cut -f2- -d "@" | rev |
-sed "s/@/NNN/g" | sed "s/ //g" | awk -F '\t' '{print $1,length($2)+6}' | sed "s/ /\t/g" > K12MG1655.getorf.bacterial.longestORFlengths.tsv
-
-awk -F '\t' '($7=="-"&&$4>0)' K12MG1655.getorf.bacterial.gtf |
-awk -F '\t' '{OFS=FS}{print $1,$2,$3,$4,$5+500,$6,$7,$8,$9}' |
-awk -F '\t' '{OFS=FS}{if ($5 > 4641652) $5 = 4641652; print}' |
-gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - |
-sed '/^>/!{ s/.*/echo "&" | rev/e }' |
-awk '{if ($0 ~ /^>/) print; else {gsub(/.{3}/, "& "); print}}' |
-cut -f2- -d " " |
-sed -E 's/AGT|AAT|GAT/%/g' |
-seqkit fx2tab | cut -f1 -d "%" |
-sed -E 's/GTA|GTG|GTT/@/g' | grep "@" |
-rev | cut -f2- -d "@" | rev |
-sed "s/@/NNN/g" | sed "s/ //g" | awk -F '\t' '{print $1,length($2)+6}' | sed "s/ /\t/g" >> K12MG1655.getorf.bacterial.longestORFlengths.tsv
-
-sed "s/(+)//g" REL606.getorf.bacterial.longestORFlengths.tsv | sed "s/(-)//g" | sort -k1 > temp
-sed "s/(+)//g" K12MG1655.getorf.bacterial.longestORFlengths.tsv | sed "s/(-)//g" | sort -k1 > temp
-
-cut -f -2 -d "\"" REL606.getorf.bacterial.gtf | sed "s/gene_id \"//g" | sort -k9 | join -1 9 -2 1 - temp | awk '($8=="+")' | awk '{print $2"\t.\tCDS\t"$6-$NF+1"\t"$6"\t.\t+\t.\ttranscript_id \""$1"\";gene_id \""$1"\";\t"$5}' | awk -F '\t' '($5-$4+1>32)' > REL606.getorf.bacterial.longest.gtf
-cut -f -2 -d "\"" REL606.getorf.bacterial.gtf | sed "s/gene_id \"//g" | sort -k9 | join -1 9 -2 1 - temp | awk '($8=="-")' | awk '{print $2"\t.\tCDS\t"$5"\t"$5+$NF-1"\t.\t-\t.\ttranscript_id \""$1"\";gene_id \""$1"\";\t"$6}' | awk -F '\t' '($5-$4+1>32)' >> REL606.getorf.bacterial.longest.gtf
-
-cut -f -2 -d "\"" K12MG1655.getorf.bacterial.gtf | sed "s/gene_id \"//g" | sort -k9 | join -1 9 -2 1 - temp | awk '($8=="+")' | awk '{print $2"\t.\tCDS\t"$6-$NF+1"\t"$6"\t.\t+\t.\ttranscript_id \""$1"\";gene_id \""$1"\";\t"$5}' | awk -F '\t' '($5-$4+1>32)' > K12MG1655.getorf.bacterial.longest.gtf
-cut -f -2 -d "\"" K12MG1655.getorf.bacterial.gtf | sed "s/gene_id \"//g" | sort -k9 | join -1 9 -2 1 - temp | awk '($8=="-")' | awk '{print $2"\t.\tCDS\t"$5"\t"$5+$NF-1"\t.\t-\t.\ttranscript_id \""$1"\";gene_id \""$1"\";\t"$6}' | awk -F '\t' '($5-$4+1>32)' >> K12MG1655.getorf.bacterial.longest.gtf
-
-awk -F '\t' '($7=="+")' REL606_all_annotated.mod.gtf | cut -f5 | sort -u | sed "s/^/\t/g" | sed "s/$/\t./g" | grep -o -F -f - REL606.getorf.bacterial.longest.gtf > stops_represented
-awk -F '\t' '($7=="-")' REL606_all_annotated.mod.gtf | cut -f4 | sort -u | sed "s/^/CDS\t/g" | sed "s/$/\t/g" | grep -o -F -f - REL606.getorf.bacterial.longest.gtf >> stops_represented
-
-awk -F '\t' '($7=="+")' K12MG1655_all_annotated.mod.gtf | cut -f5 | sort -u | sed "s/^/\t/g" | sed "s/$/\t./g" | grep -o -F -f - K12MG1655.getorf.bacterial.longest.gtf > stops_represented
-awk -F '\t' '($7=="-")' K12MG1655_all_annotated.mod.gtf | cut -f4 | sort -u | sed "s/^/CDS\t/g" | sed "s/$/\t/g" | grep -o -F -f - K12MG1655.getorf.bacterial.longest.gtf >> stops_represented
-
-grep -v -F -f stops_represented REL606_all_annotated.mod.gtf | awk -F '\t' '($7=="+")' | cut -f5 | sed "s/^/\t/g" | sed "s/$/\t./g" | sort -u > unrepresented_stops
-grep -v -F -f stops_represented REL606_all_annotated.mod.gtf | awk -F '\t' '($7=="-")' | cut -f4 | sed "s/^/CDS\t/g" | sed "s/$/\t/g" | sort -u >> unrepresented_stops
-
-grep -v -F -f stops_represented K12MG1655_all_annotated.mod.gtf | awk -F '\t' '($7=="+")' | cut -f5 | sed "s/^/\t/g" | sed "s/$/\t./g" | sort -u > unrepresented_stops
-grep -v -F -f stops_represented K12MG1655_all_annotated.mod.gtf | awk -F '\t' '($7=="-")' | cut -f4 | sed "s/^/CDS\t/g" | sed "s/$/\t/g" | sort -u >> unrepresented_stops
-
-cut -f 1-9 REL606.getorf.bacterial.longest.gtf | bedtools intersect -s -wo -a - -b REL606_all_annotated.mod.gtf | awk -F '\t' '(($7=="+")&&($5==$14))' | cut -f2 -d "\"" | sort -u | sed "s/.*/\"&\"/g" > exclude
-cut -f 1-9 REL606.getorf.bacterial.longest.gtf | bedtools intersect -s -wo -a - -b REL606_all_annotated.mod.gtf | awk -F '\t' '(($7=="-")&&($4==$13))' | cut -f2 -d "\"" | sort -u | sed "s/.*/\"&\"/g" >> exclude
-
-cut -f 1-9 K12MG1655.getorf.bacterial.longest.gtf | bedtools intersect -s -wo -a - -b K12MG1655_all_annotated.mod.gtf | awk -F '\t' '(($7=="+")&&($5==$14))' | cut -f2 -d "\"" | sort -u | sed "s/.*/\"&\"/g" > exclude
-cut -f 1-9 K12MG1655.getorf.bacterial.longest.gtf | bedtools intersect -s -wo -a - -b K12MG1655_all_annotated.mod.gtf | awk -F '\t' '(($7=="-")&&($4==$13))' | cut -f2 -d "\"" | sort -u | sed "s/.*/\"&\"/g" >> exclude
-
-cut -f 1-9 REL606.getorf.bacterial.longest.gtf | grep -v -F -f exclude > REL606.getorf.bacterial.longest.annotexcluded.gtf
-cat REL606.getorf.bacterial.longest.annotexcluded.gtf | gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - | seqkit rmdup -s - | seqkit fx2tab | sed "s/(+)//g" | sed "s/(-)//g" | sed "s/^/>/g" | sed "s/\t$//g" | sed "s/\t/\n/g" > REL606.getorf.bacterial.longest.final.faa
-cat REL606.getorf.bacterial.longest.annotexcluded.gtf | gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - | seqkit rmdup -s - | grep "^>" | tr -d ">" | cut -f1 -d "(" | sed "s/.*/\"&\"/g" | grep -F -f - REL606.getorf.bacterial.longest.annotexcluded.gtf > REL606.getorf.bacterial.longest.final.gtf
-
-cut -f 1-9 K12MG1655.getorf.bacterial.longest.gtf | grep -v -F -f exclude > K12MG1655.getorf.bacterial.longest.annotexcluded.gtf
-cat K12MG1655.getorf.bacterial.longest.annotexcluded.gtf | gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - | seqkit rmdup -s - | seqkit fx2tab | sed "s/(+)//g" | sed "s/(-)//g" | sed "s/^/>/g" | sed "s/\t$//g" | sed "s/\t/\n/g" > K12MG1655.getorf.bacterial.longest.final.faa
-cat K12MG1655.getorf.bacterial.longest.annotexcluded.gtf | gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - | seqkit rmdup -s - | grep "^>" | tr -d ">" | cut -f1 -d "(" | sed "s/.*/\"&\"/g" | grep -F -f - K12MG1655.getorf.bacterial.longest.annotexcluded.gtf > K12MG1655.getorf.bacterial.longest.final.gtf
-
+for i in K12MG1655 REL606 ECOR_11_genome ECOR_27_genome ECOR_37_genome
+do
+sed "s/(+)//g" "$i".getorf.bacterial.longestORFlengths.tsv | sed "s/(-)//g" | sort -k1 > temp
+cut -f -2 -d "\"" "$i".getorf.bacterial.gtf | sed "s/gene_id \"//g" | sort -k9 | join -1 9 -2 1 - temp | awk '($8=="+")' | awk '{print $2"\t.\tCDS\t"$6-$NF+1"\t"$6"\t.\t+\t.\ttranscript_id \""$1"\";gene_id \""$1"\";\t"$5}' | awk -F '\t' '($5-$4+1>32)' > "$i".getorf.bacterial.longest.gtf
+cut -f -2 -d "\"" "$i".getorf.bacterial.gtf | sed "s/gene_id \"//g" | sort -k9 | join -1 9 -2 1 - temp | awk '($8=="-")' | awk '{print $2"\t.\tCDS\t"$5"\t"$5+$NF-1"\t.\t-\t.\ttranscript_id \""$1"\";gene_id \""$1"\";\t"$6}' | awk -F '\t' '($5-$4+1>32)' >> "$i".getorf.bacterial.longest.gtf
+awk -F '\t' '($7=="+")' "$i"_annotated.gtf | cut -f5 | sort -u | sed "s/^/\t/g" | sed "s/$/\t./g" | grep -o -F -f - "$i".getorf.bacterial.longest.gtf > "$i"_stops_represented
+awk -F '\t' '($7=="-")' "$i"_annotated.gtf | cut -f4 | sort -u | sed "s/^/CDS\t/g" | sed "s/$/\t/g" | grep -o -F -f - "$i".getorf.bacterial.longest.gtf >> "$i"_stops_represented
+grep -v -F -f "$i"_stops_represented "$i"_annotated.gtf | awk -F '\t' '($7=="+")' | cut -f5 | sed "s/^/\t/g" | sed "s/$/\t./g" | sort -u > "$i"_unrepresented_stops
+grep -v -F -f "$i"_stops_represented "$i"_annotated.gtf | awk -F '\t' '($7=="-")' | cut -f4 | sed "s/^/CDS\t/g" | sed "s/$/\t/g" | sort -u >> "$i"_unrepresented_stops
+cut -f 1-9 "$i".getorf.bacterial.longest.gtf | bedtools intersect -s -wo -a - -b "$i"_annotated.gtf | awk -F '\t' '(($7=="+")&&($5==$14))' | cut -f2 -d "\"" | sort -u | sed "s/.*/\"&\"/g" > "$i"_exclude
+cut -f 1-9 "$i".getorf.bacterial.longest.gtf | bedtools intersect -s -wo -a - -b "$i"_annotated.gtf | awk -F '\t' '(($7=="-")&&($4==$13))' | cut -f2 -d "\"" | sort -u | sed "s/.*/\"&\"/g" >> "$i"_exclude
+cut -f 1-9 "$i".getorf.bacterial.longest.gtf | grep -v -F -f "$i"_exclude > "$i".getorf.bacterial.longest.annotexcluded.gtf
+cat "$i".getorf.bacterial.longest.annotexcluded.gtf | gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - | seqkit rmdup -s - | seqkit fx2tab | sed "s/(+)//g" | sed "s/(-)//g" | sed "s/^/>/g" | sed "s/\t$//g" | sed "s/\t/\n/g" > "$i".getorf.bacterial.longest.final.faa
+cat "$i".getorf.bacterial.longest.annotexcluded.gtf | gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - | seqkit rmdup -s - | grep "^>" | tr -d ">" | cut -f1 -d "(" | sed "s/.*/\"&\"/g" | grep -F -f - "$i".getorf.bacterial.longest.annotexcluded.gtf > "$i".getorf.bacterial.longest.final.gtf
 #Non-redundantify the annotated gtf
-
-awk -F '\t' '($7=="+")' REL606_all_annotated.mod.gtf | cut -f5 | sort -u > annotated_stops_plus
-for i in $(cat annotated_stops_plus); do awk -v var="$i" -F '\t' '($7=="+"&&$5==var)' REL606_all_annotated.mod.gtf | sort -nk 4 | head -1; done > REL606_all_annotated.nr.gtf
-awk -F '\t' '($7=="-")' REL606_all_annotated.mod.gtf | cut -f4 | sort -u > annotated_stops_minus
-for i in $(cat annotated_stops_minus); do awk -v var="$i" -F '\t' '($7=="-"&&$4==var)' REL606_all_annotated.mod.gtf | sort -nrk 5 | head -1; done >> REL606_all_annotated.nr.gtf
-
-awk -F '\t' '($7=="+")' K12MG1655_all_annotated.mod.gtf | cut -f5 | sort -u > annotated_stops_plus
-for i in $(cat annotated_stops_plus); do awk -v var="$i" -F '\t' '($7=="+"&&$5==var)' K12MG1655_all_annotated.mod.gtf | sort -nk 4 | head -1; done > K12MG1655_all_annotated.nr.gtf
-awk -F '\t' '($7=="-")' K12MG1655_all_annotated.mod.gtf | cut -f4 | sort -u > annotated_stops_minus
-for i in $(cat annotated_stops_minus); do awk -v var="$i" -F '\t' '($7=="-"&&$4==var)' K12MG1655_all_annotated.mod.gtf | sort -nrk 5 | head -1; done >> K12MG1655_all_annotated.nr.gtf
-
-cat REL606_all_annotated.nr.gtf | gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - | seqkit rmdup -s - | seqkit fx2tab | sed "s/(+)//g" | sed "s/(-)//g" | sed "s/^/>/g" | sed "s/\t$//g" | sed "s/\t/\n/g" > REL606_all_annotated.nr.seqkit.faa
-cat REL606_all_annotated.nr.gtf | gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - | seqkit rmdup -s - | grep "^>" | tr -d ">" | cut -f1 -d "(" | sed "s/.*/\"&\"/g" | grep -F -f - REL606_all_annotated.nr.gtf > REL606_all_annotated.nr.seqkit.gtf
-
-cat K12MG1655_all_annotated.nr.gtf | gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - | seqkit rmdup -s - | seqkit fx2tab | sed "s/(+)//g" | sed "s/(-)//g" | sed "s/^/>/g" | sed "s/\t$//g" | sed "s/\t/\n/g" > K12MG1655_all_annotated.nr.seqkit.faa
-cat K12MG1655_all_annotated.nr.gtf | gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - | seqkit rmdup -s - | grep "^>" | tr -d ">" | cut -f1 -d "(" | sed "s/.*/\"&\"/g" | grep -F -f - K12MG1655_all_annotated.nr.gtf > K12MG1655_all_annotated.nr.seqkit.gtf
-
-cat REL606_all_annotated.nr.seqkit.gtf REL606.getorf.bacterial.longest.final.gtf | sort -nk4 > REL606.final.gtf
-cat K12MG1655_all_annotated.nr.seqkit.gtf K12MG1655.getorf.bacterial.longest.final.gtf | sort -nk4 > K12MG1655.final.gtf
-
-cat REL606.final.gtf | gtf2bed | bedtools getfasta -s -name -fi REL606.faa -bed - > REL606.final.faa
-/stor/work/Ochman/hassan/tools/faTrans -stop REL606.final.faa REL606.final.prot.faa
-
-cat K12MG1655.final.gtf | gtf2bed | bedtools getfasta -s -name -fi K12MG1655.faa -bed - > K12MG1655.final.faa
-/stor/work/Ochman/hassan/tools/faTrans -stop K12MG1655.final.faa K12MG1655.final.prot.faa
-
+awk -F '\t' '($7=="+")' "$i"_annotated.gtf | cut -f5 | sort -u > "$i"_annotated_stops_plus
+for j in $(cat "$i"_annotated_stops_plus); do awk -v var="$j" -F '\t' '($7=="+"&&$5==var)' "$i"_annotated.gtf | sort -nk 4 | head -1; done > "$i"_annotated.nr.gtf
+awk -F '\t' '($7=="-")' "$i"_annotated.gtf | cut -f4 | sort -u > "$i"_annotated_stops_minus
+for j in $(cat "$i"_annotated_stops_minus); do awk -v var="$j" -F '\t' '($7=="-"&&$4==var)' "$i"_annotated.gtf | sort -nrk 5 | head -1; done >> "$i"_annotated.nr.gtf
+cat "$i"_annotated.nr.gtf | gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - | seqkit rmdup -s - | seqkit fx2tab | sed "s/(+)//g" | sed "s/(-)//g" | sed "s/^/>/g" | sed "s/\t$//g" | sed "s/\t/\n/g" > "$i"_all_annotated.nr.seqkit.faa
+cat "$i"_annotated.nr.gtf | gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - | seqkit rmdup -s - | grep "^>" | tr -d ">" | cut -f1 -d "(" | sed "s/.*/\"&\"/g" | grep -F -f - "$i"_annotated.nr.gtf > "$i"_all_annotated.nr.seqkit.gtf
+cat "$i"_all_annotated.nr.seqkit.gtf "$i".getorf.bacterial.longest.final.gtf | sort -nk4 > "$i".final.gtf
+cat "$i".final.gtf | gtf2bed | bedtools getfasta -s -name -fi "$i".faa -bed - > "$i".final.faa
+/stor/work/Ochman/hassan/tools/faTrans -stop "$i".final.faa "$i".final.prot.faa
+done
