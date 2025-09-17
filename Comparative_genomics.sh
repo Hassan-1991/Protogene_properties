13a81f689899b6dd5e648f50064f6f6c97f29deb
Hassan-1991 <67057211+Hassan-1991@users.noreply.github.com>
2025-09-17 12:32:00 -0500

    Delete Comparative_genomics.sh

--- Comparative_genomics.sh
+++
@@ -1,4 +0,0 @@
-#Take protogenes from E. coli, Salmonella and M. tuberculosis
-#Take "representative" samples of annotated genes (whatever that means) from all three
-#Run the comparative genomics pipeline - except add the without-synteny search as well
-
